b747a98dfa232b40fd1920a37a441702e6236a46
micheal-w-wells <micheal.w.wells@gmail.com>
2019-07-24 14:49:02 -0700

    fix to match new version of connexion and openapi 3 examples

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 from flask import (
     Flask,
-    render_template
 )
 
 import sys
@@ -11,31 +10,16 @@ import connexion_helper
 app = connexion.App(__name__, specification_dir='./')
 # Read the swagger.yml file to configure the endpoints
 spec_file_name = 'apispec.yml'
-if not(connexion_helper.load_api_spec(app, spec_file_name)):
-	print("Unable to load api spec file: " + spec_file_name + ", exiting...")
-	sys.exit()
+#if not(connexion_helper.load_api_spec(app, spec_file_name)):
+	#print("Unable to load api spec file: " + spec_file_name + ", exiting...")
+	#sys.exit()
 
-# Create a URL route in our application for "/"
-@app.route('/')
-def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
+def post_greeting(name: str) -> str:
+    return 'Hello {name}'.format(name=name)
 
-    :return:        the rendered template 'home.html'
-    """
-    return render_template('home.html')
-
-@app.route('/example')
-def exampleOperation():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
-
-    :return:        the rendered template 'home.html'
-    """
-    return 200
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
+    app = connexion.FlaskApp(__name__, port=9090, specification_dir='./')
+    app.add_api(spec_file_name, arguments={'title': 'Hello World Example'})
     app.run(debug=True)
