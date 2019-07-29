e4abf671c2f4ec176288c7a9cbfa7134f440cd7d
micheal-w-wells <micheal.w.wells@gmail.com>
2019-07-29 16:36:40 -0700

    clean up

--- hello.py
+++ hello.py
@@ -6,13 +6,7 @@ import sys
 import connexion
 import connexion_helper
 
-# Create the application instance
-app = connexion.App(__name__, specification_dir='./')
-# Read the swagger.yml file to configure the endpoints
 spec_file_name = 'apispec.yml'
-#if not(connexion_helper.load_api_spec(app, spec_file_name)):
-	#print("Unable to load api spec file: " + spec_file_name + ", exiting...")
-	#sys.exit()
 
 def post_greeting(name: str) -> str:
     return 'Hello {name}'.format(name=name)
@@ -20,6 +14,12 @@ def post_greeting(name: str) -> str:
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
+# Create the application instance
     app = connexion.FlaskApp(__name__, port=9090, specification_dir='./')
-    app.add_api(spec_file_name, arguments={'title': 'Hello World Example'})
-    app.run(debug=True)
+    try:
+# Read the swagger.yml file to configure the endpoints
+        app.add_api(spec_file_name, arguments={'title': 'Hello World Example'})
+        app.run(debug=True)
+    except Exception as e:
+        print("Unable to start the API server, encountered a problem starting up.")
+        print(e.message)
